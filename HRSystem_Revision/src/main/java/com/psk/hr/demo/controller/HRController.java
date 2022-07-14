7bd713a70ed8d1dd23b68b3dd8e778547297ee03
sangkipark <sangki931@gmail.com>
2022-07-14 23:27:37 +0900

    commit 커밋

--- HRSystem_Revision/src/main/java/com/psk/hr/demo/controller/HRController.java
+++ HRSystem_Revision/src/main/java/com/psk/hr/demo/controller/HRController.java
@@ -3,8 +3,6 @@ package com.psk.hr.demo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.transaction.Transactional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
